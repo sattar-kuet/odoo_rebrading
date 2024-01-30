94e02c73003653ff7b530c4c5d6fe6f19752602f
Naznin Akter <naznin.kuet.te@gmail.com>
2024-01-31 01:23:55 +0600

    push origin main

--- static/src/js/branding.js
+++ static/src/js/branding.js
@@ -1,26 +1,23 @@
 /** @odoo-module */
 
 export class Branding extends owl.Component {
-    setup() {
-        onWillStart(async () => {
-            console.log('hello');
-            const styleElement = document.createElement("style");
-            styleElement.textContent = `
+  setup() {
+    onWillStart(async () => {
+      console.log('hello');
+      const styleElement = document.createElement("style");
+      styleElement.textContent = `
               .o_main_navbar,
               .btn-primary {
                 background-color: #060734;
                 border-bottom: 1px solid #060734;
               }
-      
               .text-primary {
                 color: #060734 !important;
               }
             `;
-
-            this.env.head.appendChild(styleElement);
-
-            // Your other onWillStart logic goes here
-        });
-    }
+      this.env.head.appendChild(styleElement);
+      // Your other onWillStart logic goes here
+    });
+  }
 }
 Branding.template = "odoo_rebranding.Template"
